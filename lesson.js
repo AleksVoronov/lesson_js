b5c38dcfbdfac1b9e65dfae9dfd9c8a7837e347d
AleksVoronov <av591955@gmail.com>
2020-07-31 23:35:34 +0300

    next

---
+++ lesson.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+let ensors =[];
+ensors[1] = prompt('Как вас зовут?', "");
+ensors[2] = prompt('Ваша фамилия', "");
+ensors[3] = prompt('Сколько вам лет?', "");
+
+document.write(ensors);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
